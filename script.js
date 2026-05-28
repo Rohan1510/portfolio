618b95d6c994ed03f510dca0436ade00d062b1f5
Rohan1510 <143777926+Rohan1510@users.noreply.github.com>
2026-05-28 18:58:46 +0530

    Add page view tracker to script.js

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+window.addEventListener("load", () => {
+
+  fetch("https://frosty-term-384e.rohankrishna150.workers.dev/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      page: window.location.pathname,
+      referrer: document.referrer,
+      screen: `${screen.width}x${screen.height}`,
+      userAgent: navigator.userAgent,
+      timestamp: new Date().toISOString()
+    })
+  });
+
+});
